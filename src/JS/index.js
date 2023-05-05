d00fccd7f32bae44610b2264d719eaad1be74fdc
FlavioMach <flaviomachadojunior@gmail.com>
2023-05-05 01:07:39 -0300

    adicionado evento em JS de botoes selecionados e desselecionados

--- src/JS/index.js
+++ src/JS/index.js
@@ -1,1 +1,9 @@
-(document.querySelectorAll('.botao'));
+const botoesCarrossel = document.querySelectorAll('.botao');
+
+botoesCarrossel.forEach((botao) => {
+   botao.addEventListener('click', () => {
+          const botaoSelecionado =document.querySelector('.selecionado')
+            botaoSelecionado.classList.remove('selecionado');
+        botao.classList.add('selecionado');
+    })
+})
