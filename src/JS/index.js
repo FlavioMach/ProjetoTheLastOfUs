25b12b3222b794c1371898338aa56155bfc26ef9
FlavioMach <flaviomachadojunior@gmail.com>
2023-05-05 01:59:11 -0300

    add função dos botoes e troca de imagem ao clicar

--- src/JS/index.js
+++ src/JS/index.js
@@ -1,9 +1,35 @@
 const botoesCarrossel = document.querySelectorAll('.botao');
+const imagens = document.querySelectorAll('.imagem');
 
-botoesCarrossel.forEach((botao) => {
+botoesCarrossel.forEach((botao, indice) => {
    botao.addEventListener('click', () => {
-          const botaoSelecionado =document.querySelector('.selecionado')
-            botaoSelecionado.classList.remove('selecionado');
-        botao.classList.add('selecionado');
+            
+            DesativarBotaoSelecionado();
+
+            desativarBotaoSelecionado(botao);
+
+            EsconderImagemAtiva();
+            
+            mostrarImagemDeFundo(indice);
+            
+                  
     })
-})
+}) 
+
+function desativarBotaoSelecionado(botao) {
+    botao.classList.add('selecionado');
+}
+
+function mostrarImagemDeFundo(indice) {
+    imagens[indice].classList.add('ativa');
+}
+
+function DesativarBotaoSelecionado() {
+    const botaoSelecionado = document.querySelector('.selecionado');
+    botaoSelecionado.classList.remove('selecionado');
+}
+
+function EsconderImagemAtiva() {
+    const imagemAtiva = document.querySelector('.ativa');
+    imagemAtiva.classList.remove('ativa');
+}
